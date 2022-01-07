8e367182d6909ed61d942c251f5c1229eadc176f
Andre Silva <andreswebs@pm.me>
2022-01-07 09:47:45 -0300

    add outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "directory" {
+  value = {
+    id                = aws_directory_service_directory.this.id
+    dns_ip_addresses  = aws_directory_service_directory.this.dns_ip_addresses
+    security_group_id = aws_directory_service_directory.this.security_group_id
+  }
+}
+
+output "file_system" {
+  value = {
+    id                       = aws_fsx_windows_file_system.this.id
+    arn                      = aws_fsx_windows_file_system.this.arn
+    preferred_file_server_ip = aws_fsx_windows_file_system.this.preferred_file_server_ip
+  }
+}

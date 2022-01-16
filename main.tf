8b7e84bef96918b22502bad391e56d79af066eb1
Andre Silva <andreswebs@pm.me>
2022-01-16 15:45:17 -0300

    bump vers

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "fsx" {
   source  = "andreswebs/ad-fsx/aws"
-  version = "0.0.6"
+  version = "0.0.7"
   ad_name                        = var.ad_name
   vpc_id                         = var.vpc_id
   subnet_ids                     = var.subnet_ids

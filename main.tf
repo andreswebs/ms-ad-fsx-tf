af8069f232b3f09f7cb00905d4c708a5f4606b03
Andre Silva <andreswebs@pm.me>
2022-01-16 15:42:07 -0300

    fix var name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "fsx" {
   source  = "andreswebs/ad-fsx/aws"
-  version = "0.0.5"
+  version = "0.0.6"
   ad_name                        = var.ad_name
   vpc_id                         = var.vpc_id
   subnet_ids                     = var.subnet_ids
